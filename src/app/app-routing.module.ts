402c6256e8b73f1d32321a7405aab6195468c5c6
muskaanlagwal <muskaanlagwal123@gmail.com>
2019-07-20 16:08:40 +0530

    hello

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EventsComponent } from './events/events.component';
+import { MeanComponent } from './mean/mean.component';
+import { ProjectListComponent } from './project-list/project-list.component';
+import { ProjectComponent } from './project/project.component';
+import { QueryComponent } from './query/query.component';
+const routes: Routes = [
+  {path:'',component:DashboardComponent},
+  {path:'events',component:EventsComponent},
+  {path:'mean',component:MeanComponent},
+  {path: 'project-list', component:ProjectListComponent},
+  {path: 'project', component:ProjectComponent},
+  {path:'query' , component:QueryComponent}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EventsComponent } from './events/events.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MeanComponent } from './mean/mean.component';
+import { ProjectListComponent } from './project-list/project-list.component';
+import { ProjectComponent } from './project/project.component';
+import { QueryComponent } from './query/query.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    EventsComponent,
+    SidebarComponent,
+    MeanComponent,
+    ProjectListComponent,
+    ProjectComponent,
+    QueryComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
